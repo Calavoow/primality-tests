657381a74d69f53169374f50e37c6be7fc342886
Calavoow <Calavoow@users.noreply.github.com>
2014-03-18 15:20:38 +0100

    Written introduction section.

--- lab1/src/main/scala/main.scala
+++ lab1/src/main/scala/main.scala
@@ -5,17 +5,18 @@ object Main {
   import randomized._
 
   def main(args: Array[String]) {
-    for(i<- 4 to 1000) {
-      val s = Primality.test(i, 2, solovayStrassenTest)
-      val m = Primality.test(i, 2, millerRabinTest)
-      val a = AKS(i)
-      (s,m,a) match {
-        case (ProbablyPrime, _, Composite) => print("F")
-        case (_, ProbablyPrime, Composite) => print("F")
-        case (Composite, _, Prime) => print("E")
-        case (_, Composite, Prime) => print("E")
-        case _ => print(".")
-      }
-    }
+    Testing.accuracyTest
+//    for(i<- 4 to 1000) {
+//      val s = Primality.test(i, 2, solovayStrassenTest)
+//      val m = Primality.test(i, 2, millerRabinTest)
+//      val a = AKS(i)
+//      (s,m,a) match {
+//        case (ProbablyPrime, _, Composite) => print("F")
+//        case (_, ProbablyPrime, Composite) => print("F")
+//        case (Composite, _, Prime) => print("E")
+//        case (_, Composite, Prime) => print("E")
+//        case _ => print(".")
+//      }
+//    }
   }
 }
