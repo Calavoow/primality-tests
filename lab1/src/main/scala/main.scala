9012faec8f63a3b32c1c158008e2344c3aafb23d
A.J. Rouvoet <a.j.rouvoet@gmail.com>
2014-03-18 13:56:18 +0100

    Updated main to test AKS

--- lab1/src/main/scala/main.scala
+++ lab1/src/main/scala/main.scala
@@ -1,16 +1,19 @@
-import randomized.Primality
+import randomized.Primality._
 
 object Main {
 
-  import randomized.Primality._
+  import randomized._
 
   def main(args: Array[String]) {
     for(i<- 4 to 1000) {
       val s = Primality.test(i, 2, solovayStrassenTest)
       val m = Primality.test(i, 2, millerRabinTest)
-      (s,m) match {
-        case (ProbablyPrime, Composite) => print("S")
-        case (Composite, ProbablyPrime) => print("M")
+      val a = AKS(i)
+      (s,m,a) match {
+        case (ProbablyPrime, _, Composite) => print("F")
+        case (_, ProbablyPrime, Composite) => print("F")
+        case (Composite, _, Prime) => print("E")
+        case (_, Composite, Prime) => print("E")
         case _ => print(".")
       }
     }
