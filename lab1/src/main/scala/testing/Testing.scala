662ae141b3e4a2ff18f191804aca0780a95d9159
A.J. Rouvoet <a.j.rouvoet@gmail.com>
2014-03-18 15:20:39 +0100

    Cleaned up testing map to Set made results corrupt

--- lab1/src/main/scala/testing/Testing.scala
+++ lab1/src/main/scala/testing/Testing.scala
@@ -1,84 +1,85 @@
 import java.io.{File, PrintWriter}
-import randomized.AKS
 import randomized.Primality._
 import scala.io.Source
 
 object Testing {
 
-  def accuracyTest = {
+  // list of primes
+  val primes: List[Int] = (Source.fromFile("src/main/resources/primesBelow1000.txt").getLines() map {
+    _.toInt
+  }).toList
+
+  // list of composites
+  val composites: List[Int] = (3 to 7919).toList filter {
+    i => i % 2 != 0 && !primes.contains(i)
+  }
+
+  def accuracyTest() = {
     def calcAccuracy(primeTester: Int => Outcome, runs: Int) = {
-      val results : Set[(Int,Int)] = numbers.map((number) => (number, outcomesAlgo(number, primeTester, runs)))
-      val tmp = results.map(_._2)
-      (mean(tmp), sample_stddev(tmp))
+      val results = composites.map {
+        (number) => outcomesAlgo(number, primeTester, runs)
+      }
+
+      (mean(results), sample_stddev(results))
     }
 
     val runs = 100
     println(s"Testing accuracy $runs times for all composite numbers between 3 and 7919")
-    val solovayResults = calcAccuracy(solovayStrassenTest _, runs)
+
+    val solovayResults = calcAccuracy(solovayStrassenTest, runs)
     println(s"Solovay times Prime: mean ${solovayResults._1}, stddev ${solovayResults._2}")
-    val millerResults = calcAccuracy(millerRabinTest _, runs)
+
+    val millerResults = calcAccuracy(millerRabinTest, runs)
     println(s"Miller times Prime: mean ${millerResults._1}, stddev ${millerResults._2}")
   }
 
   def runTimeTest() = {
-    val logInput = for(i <- 0.5 to 6.0 by 0.1) yield {
-      val result = Math.round(Math.pow(10, i)).toInt  // They need to be whole numbers
+    val logInput = for (i <- 0.5 to 6.0 by 0.1) yield {
+      val result = Math.round(Math.pow(10, i)).toInt // They need to be whole numbers
       // Make sure they are not even.
-      if( result%2 == 0 ) result+1
+      if (result % 2 == 0) result + 1
       else result
     }
 
-    def calcRunTime(primeTester: Int => Outcome, runs: Int) : Map[Int, (Double, Double)] = {
+    def calcRunTime(primeTester: Int => Outcome, runs: Int): Map[Int, (Double, Double)] = {
       // Can you do this better? Create a Map with the input to the results.
       (logInput zip logInput
-        .map(timeAlgo( _, primeTester, runs))
+        .map(timeAlgo(_, primeTester, runs))
         .map {
-            (runTimes) => (mean(runTimes), sample_stddev(runTimes))
-        }
-      ).toMap
+        (runTimes) => (mean(runTimes), sample_stddev(runTimes))
+      }
+        ).toMap
     }
 
     val runs = 100
     println(s"Testing runtime $runs times for logarithmically scaled inputs between 10^(0.5) to 10^6")
-    val solovayResults = calcRunTime(solovayStrassenTest _, runs)
+    val solovayResults = calcRunTime(solovayStrassenTest, runs)
     println(s"Solovay runTimes: $solovayResults")
-    val millerResults = calcRunTime(millerRabinTest _, runs)
+    val millerResults = calcRunTime(millerRabinTest, runs)
     println(s"Miller runTimes: $millerResults")
-//    val aksResults = calcRunTime(AKS.apply _, runs)
-//    println(s"AKS runTimes: $aksResults")
+    //    val aksResults = calcRunTime(AKS.apply _, runs)
+    //    println(s"AKS runTimes: $aksResults")
 
 
     // Write results to CSV
     val resultWriter = new PrintWriter(new File("src/main/resources/runtimeResults.csv"))
     resultWriter.println("input,solovay mean,solovay stddev,miller mean, miller stddev")
-    for(input <- logInput) {
+    for (input <- logInput) {
       val resultLine = input :: solovayResults(input).productIterator.toList ::: millerResults(input).productIterator.toList
       resultWriter.println(resultLine.mkString(","))
     }
     resultWriter.close()
   }
 
-  /**
-   * Create a list of Composite numbers that are not even.
-   */
-  def numbers = {
-    val lines = Source.fromFile("src/main/resources/primesBelow1000.txt").getLines()
-    val primes = (for(line <- lines) yield line.toInt).toSet
-    (
-      for(i <- 3 to 7919
-          if i%2 != 0 // Skip even numbers
-      ) yield i).toSet -- primes
-  }
-
-  def mean[T](items:Traversable[T])(implicit num: Numeric[T]) = {
+  def mean[T](items: Traversable[T])(implicit num: Numeric[T]) = {
     num.toDouble(items.sum) / items.size
   }
 
-  def sample_stddev[T](items:Traversable[T])(implicit num: Numeric[T]) = {
+  def sample_stddev[T](items: Traversable[T])(implicit num: Numeric[T]) = {
     val meanCalc = mean(items)
     Math.sqrt(
-      (1/(items.size.toDouble-1)) * items.foldLeft(0.0) {
-        (sum : Double, item) => sum + Math.pow(num.toDouble(item) - meanCalc, 2)
+      (1 / (items.size.toDouble - 1)) * items.foldLeft(0.0) {
+        (sum: Double, item) => sum + Math.pow(num.toDouble(item) - meanCalc, 2)
       }
     )
   }
@@ -88,8 +89,8 @@ object Testing {
    *
    * This is not suitable for testing AKS, because it does not have an error (it return either Composite or Prime).
    */
-  def outcomesAlgo(input: Int, algo: Int => Outcome, times : Int) : Int = {
-    (for(i <- 1 to times) yield {
+  def outcomesAlgo(input: Int, algo: Int => Outcome, times: Int): Int = {
+    (for (i <- 1 to times) yield {
       algo(input)
     }).foldLeft(0) {
       case (primeTimes, ProbablyPrime) => primeTimes + 1
@@ -100,8 +101,8 @@ object Testing {
   /**
    * Returns the runtimes in nanoseconds of the algorithm for the given input, tested times number.
    */
-  def timeAlgo(input: Int, algo: Int => Outcome, times: Int) : Seq[Long] = {
-    for(i <- 1 to times) yield {
+  def timeAlgo(input: Int, algo: Int => Outcome, times: Int): Seq[Long] = {
+    for (i <- 1 to times) yield {
       val startTime = System.nanoTime
       algo(input)
       System.nanoTime - startTime
