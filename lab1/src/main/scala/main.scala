33ce855acb373174678866bbdf778dcfbfab1d3e
A.J. Rouvoet <a.j.rouvoet@gmail.com>
2014-03-19 22:04:46 +0100

    Cleaned up main to test compute all numbers upto 10^9 fast With low reliability

--- lab1/src/main/scala/main.scala
+++ lab1/src/main/scala/main.scala
@@ -5,18 +5,12 @@ object Main {
   import randomized._
 
   def main(args: Array[String]) {
-    Testing.runTimeTest()
-//    for(i<- 4 to 1000) {
-//      val s = Primality.test(i, 2, solovayStrassenTest)
-//      val m = Primality.test(i, 2, millerRabinTest)
-//      val a = AKS(i)
-//      (s,m,a) match {
-//        case (ProbablyPrime, _, Composite) => print("F")
-//        case (_, ProbablyPrime, Composite) => print("F")
-//        case (Composite, _, Prime) => print("E")
-//        case (_, Composite, Prime) => print("E")
-//        case _ => print(".")
-//      }
-//    }
+    for(i<- 3 to Math.pow(10, 9).toInt) {
+      val s = Primality.test(i, 3, millerRabinTest)
+      s match {
+        case ProbablyPrime => println(s"$i: PRIME")
+        case Composite => println(s"$i: Composite")
+      }
+    }
   }
 }
